1754467de16173036bdb8ec4a5105eb9b75150d5
Roby <goatkecske1512@gmail.com>
2026-01-21 12:10:49 +0100

    Refactor Roby functions for better error handling

--- client/functions.lua
+++ client/functions.lua
@@ -1,14 +1,37 @@
-Roby.isHeadBone = function(boneId)
-    return Roby.WeaponDamage.HeadBones[boneId] == true
+if Roby == nil then Roby = {} end
+local blockedHashes = {}
+
+Roby.CacheHashes = function()
+    blockedHashes = {} 
+    if Roby.WeaponDamage and Roby.WeaponDamage.BlockedHeadshotWeapons then
+        for weaponName, _ in pairs(Roby.WeaponDamage.BlockedHeadshotWeapons) do
+            if type(weaponName) == "string" then
+                blockedHashes[GetHashKey(weaponName)] = true
+            else
+                blockedHashes[weaponName] = true
+            end
+        end
+    else
+        print("~1[Roby Damage] Error: Roby.WeaponDamage or Roby.WeaponDamage.BlockedHeadshotWeapons is nil.~7")
+    end
 end
 
 Roby.isWeaponBlockedForHeadshot = function(weaponHash)
-    return Roby.WeaponDamage.BlockedHeadshotWeapons[weaponHash] == true
+    return blockedHashes[weaponHash] == true
+end
+
+Roby.isHeadBone = function(boneId)
+    if Roby.WeaponDamage and Roby.WeaponDamage.HeadBones then
+        return Roby.WeaponDamage.HeadBones[boneId] == true
+    end
+    return false
 end
 
 Roby.applyAllMultipliers = function()
-    for weaponName, mult in pairs(Roby.WeaponDamage.Multipliers) do
-        local h = GetHashKey(weaponName)
-        SetWeaponDamageModifier(h, mult)
+    if Roby.WeaponDamage and Roby.WeaponDamage.Multipliers then
+        for weaponName, mult in pairs(Roby.WeaponDamage.Multipliers) do
+            local h = GetHashKey(weaponName)
+            SetWeaponDamageModifier(h, mult)
+        end
     end
 end
